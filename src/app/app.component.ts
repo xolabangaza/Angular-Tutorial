b557d1942f99de0206ae2cd05dc9b905b3d61cc4
Xola Bangaza <xolabangaza@gmail.com>
2024-05-03 14:39:01 +0200

    d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'x-app';
+  title = 'Tour of Heroes';
 }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import {HEROES} from '../mock-heroes';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  heroes = HEROES;
+     hero: Hero ={
+      id: 1,
+      name: 'Windstorm' 
+      
+     };
+     
+}
+
+
